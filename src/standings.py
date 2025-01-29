9b8ccf8e66ce411ff00061476bb0394c47fa04c0
mwoithe <133837694+mwoithe@users.noreply.github.com>
2025-01-29 12:27:33 +1030

    CSS changes

--- src/standings.py
+++ src/standings.py
@@ -95,7 +95,7 @@ class Standings:
 
     <meta charset="utf-8">
     <link rel="stylesheet" href="display.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="constant.css">
 </head>
 
 <body>
